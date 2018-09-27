a4b34ebf35d6bf5cc793ab282b484988be3ec0c1
Sam Morrell <samuelmorrell@gmail.com>
2018-09-27 01:53:20 +0100

    Added command line argument to the main function foir config file.

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,21 @@ extern crate tesys_derive;
 mod tesys;
 use tesys::astrometry::Angle;
 use tesys::Peer;
+use std::env;
 
-fn main() {
+fn main() -> Result<(), std::io::Error> {
+
+	// Let's first check and see if we have a config file as a command line argument
+	if env::args().count() >= 2 {
+		// We have one, so load it and away we go.
+		let conf_file = env::args().nth(1).unwrap();
+		tesys::loggable::log(&format!("Loading configuration: {}", conf_file));
+	} else {
+		// We don't have one, so display an error.
+		tesys::loggable::err("No configuration file specified. ");
+		tesys::loggable::err("Please give the configuration file as the first argument. ");
+	}
+
     tesys::loggable::log("Starting Tesys...");
     let mut _an: Angle = Angle::new(12.0);
     let _hms = _an.to_hms();
@@ -16,4 +29,5 @@ fn main() {
 
     tesys::loggable::log("Initialising Peer...");
     let _p = Peer::new();
+	Ok(())
 }
