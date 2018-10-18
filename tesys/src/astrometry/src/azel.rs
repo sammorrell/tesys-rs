2a5a730ef5f3077985eeac2c8c16f85868eb8e78
Sam Morrell <samuelmorrell@gmail.com>
2018-10-19 00:17:13 +0100

    Improved basic implementation of AzEl struct.

--- tesys/src/astrometry/src/azel.rs
+++ tesys/src/astrometry/src/azel.rs
@@ -1,11 +1,13 @@
+use angle::Angle;
+
 pub struct AzEl {
-	az: Angle,
-	el: Angle,
+	pub az: Angle,
+	pub el: Angle,
 }
 
 impl AzEl {
-	pub fn new (az: f32, el: f32) {
-		Angle {
+	pub fn new (az: f32, el: f32) -> AzEl {
+		AzEl {
 			az: Angle::new(az),
 			el: Angle::new(el),
 		}
