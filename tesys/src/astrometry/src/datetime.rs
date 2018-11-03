51a810527c95160a1fa5a614eba8f70265a29ace
Sam Morrell <samuelmorrell@gmail.com>
2018-11-03 18:06:27 +0000

    Moved DateTime from Local to Utc offset, because that makes much more sense.

--- tesys/src/astrometry/src/datetime.rs
+++ tesys/src/astrometry/src/datetime.rs
@@ -1,23 +1,24 @@
-use chrono::{DateTime, Local};
+use chrono::{DateTime, Utc};
 use Angle;
 use Location;
 
-const SECONDS_PER_SIDEREAL_DAY: f64 = 24.0 * 3600.0; // The number of seconds in 24 hours, given in f64. 
-const SECONDS_PER_SIDEREAL_YEAR: f64 = SECONDS_PER_SIDEREAL_DAY * 365.25636; // The number of seconds in a sidereal year, given in f64. 
-const MJD_OFFSET: f64 = 2400000.5; // The offset between Julian Date and Mdofied Julian Date, represented in Julian days. Given in f64.
-const UNIX_EPOCH: f64 = 2440587.5; // The time of the UNIX Epoch (1970-01-01 12:00:00) in Julian days. 
+pub const SECONDS_PER_SIDEREAL_DAY: f64 = 24.0 * 3600.0; // The number of seconds in 24 hours, given in f64. 
+pub const DAYS_PER_SIDEREAL_YEAR: f64 = 365.25636;
+pub const SECONDS_PER_SIDEREAL_YEAR: f64 = SECONDS_PER_SIDEREAL_DAY * DAYS_PER_SIDEREAL_YEAR; // The number of seconds in a sidereal year, given in f64. 
+pub const MJD_OFFSET: f64 = 2400000.5; // The offset between Julian Date and Mdofied Julian Date, represented in Julian days. Given in f64.
+pub const UNIX_EPOCH: f64 = 2440587.5; // The time of the UNIX Epoch (1970-01-01 12:00:00) in Julian days. 
 
-pub fn datetime_to_julian_date(_dt: DateTime<Local>) -> f64 {
+pub fn datetime_to_julian_date(_dt: DateTime<Utc>) -> f64 {
     //! Converts a chrono::DateTime to a Julian Date in f64 format. 
     UNIX_EPOCH + ( _dt.timestamp() as f64 / SECONDS_PER_SIDEREAL_DAY )
 }
 
-pub fn datetime_to_modified_julian_date(_dt: DateTime<Local>) -> f64 {
+pub fn datetime_to_modified_julian_date(_dt: DateTime<Utc>) -> f64 {
 	//! Converts a chrono::DateTime to a Julian Date in f64 format.
     datetime_to_julian_date(_dt) - MJD_OFFSET
 }
 
-pub fn datetime_to_gmst(_dt: DateTime<Local>) -> Angle {
+pub fn datetime_to_gmst(_dt: DateTime<Utc>) -> Angle {
 	//! Converts a datetime to a Greenwich Mean Sidereal Time. 
 	let jd = datetime_to_julian_date(_dt);
 	let t = (jd - 2451545.0) / (36525.0);
@@ -28,7 +29,7 @@ pub fn datetime_to_gmst(_dt: DateTime<Local>) -> Angle {
 	Angle::new(st)
 }
 
-pub fn get_sidereal_time(_dt: DateTime<Local>, _loc: Location) -> Angle {
+pub fn get_sidereal_time(_dt: DateTime<Utc>, _loc: Location) -> Angle {
 	//! Converts a Location and a chrono::Local into Local Mean Sidereal Time. 
 	let lon: f64 = if _loc.lon.deg() < 0.0 { -360.0 - (_loc.lon.deg() % 360.0 as f64) } else { _loc.lon.deg() };
 	let lmst: f64 = ((datetime_to_gmst(_dt).deg() - lon) % 360.).into();
