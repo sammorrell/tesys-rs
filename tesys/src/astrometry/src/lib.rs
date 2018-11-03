53b74f6368c993e7ad6d2647f02297226613486c
Sam Morrell <samuelmorrell@gmail.com>
2018-11-03 18:05:38 +0000

    Added new geometric constants to support new conversion code.

--- tesys/src/astrometry/src/lib.rs
+++ tesys/src/astrometry/src/lib.rs
@@ -4,12 +4,17 @@ extern crate chrono;
 const DEG_PER_RAD: f64 = { 180.0 / std::f64::consts::PI };
 const HOUR_PER_DEG: f64 = { 1.0 / 15.0 };
 const HOUR_PER_RAD: f64 = { HOUR_PER_DEG * DEG_PER_RAD };
+const ARCSEC_PER_DEG: f64 = { 3600.0 };
+const ARCSEC_PER_RAD: f64 = { ARCSEC_PER_DEG * DEG_PER_RAD };
+const MAS_PER_DEG: f64 = { ARCSEC_PER_DEG * 1000.0 };
+const MAS_PER_RAD: f64 = { ARCSEC_PER_RAD * 1000.0 };
 
 const DEFAULT_WRAP_MAX_ANGLE: f64 = 2.0 * std::f64::consts::PI;
 const DEFAULT_WRAP_MIN_ANGLE: f64 = 0.0;
 
 pub mod epoch;
 pub use epoch::Epoch;
+
 pub mod datetime;
 
 mod angle;
