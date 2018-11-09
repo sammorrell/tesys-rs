f0b4616cb7b4f094f3902c34f4d2ca3c133cc8d9
Sam Morrell <samuelmorrell@gmail.com>
2018-11-09 22:19:53 +0000

    Added transform.rs, which is the transform builder ignored by the previous commit.

---
+++ tesys/src/astrometry/src/transform.rs
@@ -0,0 +1,37 @@
+use Location;
+use SkyCoordinate;
+use frame::Frame;
+use chrono::{DateTime, Utc};
+
+#[derive(Clone)]
+pub struct CoordinateTransform<F: Frame, T: Frame> {
+	pub coords: SkyCoordinate<F>,
+	pub from: F,
+	pub to: T,
+	pub at: DateTime<Utc>,
+	pub loc: Location,
+}
+
+impl<F: Frame, T: Frame> CoordinateTransform<F, T> {
+	pub fn new(coords: SkyCoordinate<F>) -> CoordinateTransform<F, T> {
+		CoordinateTransform {
+			coords: coords,
+			from: F::new(), 
+			to: T::new(),
+			at: Utc::now(),
+			loc: Location::new(0.0, 0.0)
+		}
+	}
+
+	pub fn at(self, _dt: DateTime<Utc>) -> CoordinateTransform<F, T> {
+		let mut ret = self.clone();
+		ret.at = _dt;
+		ret
+	}
+
+	pub fn with_location(self, _loc: Location) -> CoordinateTransform<F, T> {
+		let mut ret = self.clone();
+		ret.loc = _loc;
+		ret
+	}
+}
