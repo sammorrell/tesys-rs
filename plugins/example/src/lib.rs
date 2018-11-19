c13d2804bb794a0a083430c10e86f5f609ae80d5
smorrell <smorrell@astro.ex.ac.uk>
2018-11-19 00:36:30 +0000

    Updated example plugin code with new macros for boilerplate code.

--- plugins/example/src/lib.rs
+++ plugins/example/src/lib.rs
@@ -1,3 +1,4 @@
+#[macro_use]
 extern crate tesys;
 #[macro_use]
 extern crate tesys_derive;
@@ -6,12 +7,11 @@ use tesys::astrometry::frames::ICRS;
 use tesys::loggable::*;
 use tesys::Plugin;
 
-#[no_mangle]
-pub extern "C" fn _create_plugin() -> *mut Plugin {
-    let obj = ExamplePlugin::new();
-    let boxed: Box<ExamplePlugin> = Box::new(obj);
-    Box::into_raw(boxed)
-}
+// We call into the macros to write the extern C functions
+// which allow us to easily create and destroy our Rust
+// plugin struct. 
+tesys_plugin_create!(ExamplePlugin);
+tesys_plugin_destroy!(ExamplePlugin);
 
 #[allow(dead_code)]
 #[derive(Loggable)]
