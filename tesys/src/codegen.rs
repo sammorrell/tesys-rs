b4b480d75057eaa01420c3120744562fce9e68d6
smorrell <smorrell@astro.ex.ac.uk>
2018-12-31 23:37:01 +0000

    Cleaned tesys::codegen and suppressed warnings.

--- tesys/src/codegen.rs
+++ tesys/src/codegen.rs
@@ -1,14 +1,6 @@
-use std::collections::HashMap;
-
-use crate::Plugin;
-
 type StaticHandler<P,R> = fn(&mut P) -> Result<R, ()>;
 
-#[derive(Clone)]
-pub struct PluginVTable<P> {
-	pub _vtbl: HashMap<String, StaticHandler<P, ()>>,
-}
-
+#[allow(dead_code)]
 #[derive(Clone,Debug)]
 pub struct StaticHandlerInfo<T> {
 	pub name: &'static str,
