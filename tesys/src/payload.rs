f192a722a0cbb08923fd1f3640e0cfde860ca344
smorrell <smorrell@astro.ex.ac.uk>
2018-12-31 23:36:06 +0000

    Fleshed out tesys::Payload trait.

--- tesys/src/payload.rs
+++ tesys/src/payload.rs
@@ -1,6 +1,25 @@
 use std::fmt::Debug;
 use std::any::Any;
 
-pub trait Payload: Any + Send + Sync + Debug {
+pub trait Payload: Any + Send + Sync + Debug + PayloadClone {
 	
+}
+
+pub trait PayloadClone {
+    fn clone_payload(&self) -> Box<Payload>;
+}
+
+impl<T> PayloadClone for T
+where
+    T: 'static + Payload + Clone,
+{
+    fn clone_payload(&self) -> Box<Payload> {
+        Box::new(self.clone())
+    }
+}
+
+impl Clone for Box<Payload> {
+    fn clone(&self) -> Self {
+        self.clone_payload()
+    } 
 }
