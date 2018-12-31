4fbeb02074764ec06e98a6678a9a37ec5b3d23fe
smorrell <smorrell@astro.ex.ac.uk>
2018-12-31 23:35:39 +0000

    Worked on rough implementation for tesys::Router struct.

--- tesys/src/router.rs
+++ tesys/src/router.rs
@@ -1,18 +1,59 @@
 extern crate slotmap;
 // use slotmap::{SlotMap, DefaultKey};
-// use std::sync::mpsc::{channel, Receiver, Sender};
+use std::sync::mpsc::{channel, Receiver, Sender};
+use std::collections::{HashMap,VecDeque};
 
-use crate::Routable;
+use crate::{Routable,Message};
+
+pub type Inlet = Receiver<Message>;
+pub type Outlet = Sender<Message>;
 
 pub struct Router {
+	_receiver: Inlet,
+	_sender: Outlet,
+	outlets: HashMap<String, Outlet>,
+	root: Option<Inlet>,
 }
 
 impl Router {
 	pub fn new() -> Router {
-		Router {}
+		let (sender, receiver) = channel();
+		Router {
+			_sender: sender,
+			_receiver: receiver,
+			outlets: HashMap::new(),
+			root: None,
+		}
 	}
 
-	pub fn register<T: Routable>(_item: T) {
+	pub fn register<T: Routable>(&mut self, item: &mut T) {
+		// First we spawn a new channel and pass the Receiver to to the Routable object. 
+		let (sender, receiver) = channel();
+		item.set_inlet(receiver);
 
+		// Next we insert the outlet from the router into our outlets HashMap. 
+		self.outlets.insert(item.get_handle(), sender);
+	}
+
+	pub fn set_root(&mut self, root_inlet: Inlet) {
+		self.root = Some(root_inlet);
+	}
+
+	pub fn poll(&mut self) {
+		let mut queue = VecDeque::<Message>::new();
+
+		// First we collect all of the messages waiting for dispatch
+		for inl in self._receiver.try_iter() {
+			queue.push_back(inl);
+		}
+
+		// Now we dispatch them
+		for m in queue.iter() {
+			self.dispatch(m.clone());
+		}
+	}
+
+	pub fn dispatch(&mut self, m: Message) {
+		println!("{:?}", m);
 	}
 }
